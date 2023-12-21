560cece5b650b813d1551323106aaa87d0792209
Marlon Orpiada <134477606+marlon1412@users.noreply.github.com>
2023-12-22 01:20:45 +0800

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,72 @@
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<footer class="footer-distributed">
+
+        <div class="footer-left">
+          
+  
+         
+          <h5 class="connect">Quick Links</h5>
+          <p class="footer-links">
+            
+            
+            <a href="home.php"><i class="fa-solid fa-house"></i>  Home</a>
+            
+            <a href="about.php"><i class="fa-solid fa-address-card"></i> About</a>
+            
+            <a href="menu.php"><i class="fa-solid fa-fish-fins"></i> Fish</a>
+          
+            <a href="contact.php"><i class="fa-solid fa-square-phone"></i> Contact</a>
+            
+            
+          </p>
+  
+          
+        </div>
+  
+        <div class="footer-center">
+          <h5 class="connect">Quick Contacts</h5>
+  
+          <div>
+            <i class="fa-solid fa-map-location-dot"></i>
+            <p><span>9P5J+6X7,</span>  Tabaco City, Albay</p>
+          </div>
+  
+          <div>
+            <i class="fa fa-phone"></i>
+            <p>+63 912 635 8253</p>
+          </div>
+  
+          <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="mailto:markjosephbraga@gmail.com">Email:markjosephbraga@gmail.com</a></p>
+          </div>
+  
+        </div>
+  
+        <div class="footer-right">
+        
+         
+  
+          <div class="footer-icons">
+            <h5 class="connect">Connect</h5>
+  
+            <a href="#" target="_blank"><div class="fb"><i class="fa-brands fa-facebook"></i></div></a>
+            <a href="#" target="_blank"><div class="tw"><i class="fa-brands fa-x-twitter"></i></div></a>
+            <a href="#" target="_blank"><div class="youtube"><i class="fa-brands fa-youtube"></i></div></a>
+            <a href="#" target="_blank"><div class="store"><i class="fa-solid fa-store"></i></div></a>
+            <a href="#" target="_blank"><div class="ig"><i class="fa-brands fa-instagram"></i></div></a>
+           
+          </div>
+  
+        </div>
+        <div class="divi">    
+              <hr>
+          <p class="footer--name">All rights reserved  © Copyright 2023 </p>
+  </div>
+  
+      </footer>
+
+<!-- <div class="loader">
+   <img src="images/loader1.gif" alt="">
+</div> -->
